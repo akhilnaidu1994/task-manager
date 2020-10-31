c8b5fd0f2a7b342260ae140443a6621351308760
Akhil Naidu <akhiln1994@gmail.com>
2020-10-31 13:28:58 +0530

    changed table name

--- src/main/java/com/akn/taskmanager/model/User.java
+++ src/main/java/com/akn/taskmanager/model/User.java
@@ -7,8 +7,10 @@ import org.hibernate.annotations.GenericGenerator;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "users")
 public class User {
 
     @GeneratedValue(generator="system-uuid")
